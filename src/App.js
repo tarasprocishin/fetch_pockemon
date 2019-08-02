54d00c1318b00b8fcfb62a8fb2bd9c95366e756b
tarasprocishin <taras1procishin@gmail.com>
2019-08-02 18:59:14 +0300

    Implement solution

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      name: '', 
+      img: ''
+    }
+  }
+
+  getPokemon = async () => {
+    const randomNumber = Math.floor(Math.random() * 807 + 1);
+
+    const respose = await fetch('https://pokeapi.co/api/v2/pokemon/' + randomNumber +'/');
+    const pockemon = await respose.json();
+
+    return pockemon;
+  }
+
+ async componentDidMount() {
+     const pokemon = await this.getPokemon().then((data) =>{
+       return data
+     })
+     console.log(pokemon)
+     this.setState({name: pokemon.name ,
+                    img: pokemon.sprites.front_default})
+    }
+
+  render() {
+    let { name, img } = this.state
+ 
+  
+      return (
+          <div className="pokemon">
+            <img src = {img} alt="pokemon"/>
+              <p className="pockemon__name">
+                {name}
+              </p>
+          </div>
+      );
+  }
 }
+
+
 
 export default App;
