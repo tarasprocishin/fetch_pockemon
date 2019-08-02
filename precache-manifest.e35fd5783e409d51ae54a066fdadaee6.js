3c77e0fcd0130fdb547bf369873cdcb221481f22
tarasprocishin <taras1procishin@gmail.com>
2019-08-02 19:09:29 +0300

    Updates

---
+++ precache-manifest.e35fd5783e409d51ae54a066fdadaee6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f80fe7d564d232ab2b8dccada333eb3",
+    "url": "/fetch_pockemon/index.html"
+  },
+  {
+    "revision": "af037dbd863c6313dd23",
+    "url": "/fetch_pockemon/static/css/main.3e6b36e3.chunk.css"
+  },
+  {
+    "revision": "0f1c5798ca0a25facf8c",
+    "url": "/fetch_pockemon/static/js/2.d5ed7b26.chunk.js"
+  },
+  {
+    "revision": "af037dbd863c6313dd23",
+    "url": "/fetch_pockemon/static/js/main.ee04454d.chunk.js"
+  },
+  {
+    "revision": "fd5da3e1cd94f215af02",
+    "url": "/fetch_pockemon/static/js/runtime~main.01a65550.js"
+  }
+]);
